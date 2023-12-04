6cffb40e53d06d05130298e46ff56d05d7adf551
ubuendia001 <urkobuendia@gmail.com>
2023-12-04 16:05:43 +0100

    Creación de colores

---
+++ colores.py
@@ -0,0 +1,7 @@
+misColores=['salmon','skyblue','orange','green','darkgrey','mediumorchid','lime','tan','navy','maroon','blue','red']
+
+coloresEVE={'VerdeEVE':(93,163,102),'AzulEVE':(0,93,181),'NegroEVE':(26,23,27),'GrisEVE':(238,238,238),
+            'Industria': (114,199,216),'Transporte':(239,122,152),'Primario':(169,197,165),'Servicios':(254,208,99),'Residencial':(198,143,191),'Generación eléctrica':(244,154,115),
+            'Petróleo y derivados':(215,185,191),'Carbón y derivados':(136,159,155),'Gas natural':(189,227,242),'Energías derivadas':(241,239,173),'Renovables':(167,205,118),'Electricidad':(119,157,79),
+            'Eólica':(163,196,206),'Fotovoltaica':(250,187,105),'Hidroeléctrica':(114,199,216),'Biomasa':(199,214,168),'Biocarburantes':(224,195,148),'Solar':(255,232,122),'Térmicas avanzadas':(185,165,167),'Térmicas convencionales':(228,198,150),'Cogeneración':(248,192,186),'Geotérmica':(160,181,213),'Marina':(81,169,189),
+            'GLP':(163,196,206),'Gasolinas':(250,187,105),'Queroseno':(114,199,216),'Gasóleo A':(199,214,168),'Gasóleo B y C':(224,195,148),'Fuelóleo':(255,232,122),'Otros':(185,165,167)}
--- general.py
+++ general.py
@@ -4,15 +4,6 @@ import urllib.request as req
 import ssl
 import datetime
 
-
-misColores=['salmon','skyblue','orange','green','darkgrey','mediumorchid','lime','tan','navy','maroon','blue','red']
-
-coloresEVE={'VerdeEVE':(93,163,102),'AzulEVE':(0,93,181),'NegroEVE':(26,23,27),'GrisEVE':(238,238,238),
-            'Industria': (114,199,216),'Transporte':(239,122,152),'Primario':(169,197,165),'Servicios':(254,208,99),'Residencial':(198,143,191),'Generación eléctrica':(244,154,115),
-            'Petróleo y derivados':(215,185,191),'Carbón y derivados':(136,159,155),'Gas natural':(189,227,242),'Energías derivadas':(241,239,173),'Renovables':(167,205,118),'Electricidad':(119,157,79),
-            'Eólica':(163,196,206),'Fotovoltaica':(250,187,105),'Hidroeléctrica':(114,199,216),'Biomasa':(199,214,168),'Biocarburantes':(224,195,148),'Solar':(255,232,122),'Térmicas avanzadas':(185,165,167),'Térmicas convencionales':(228,198,150),'Cogeneración':(248,192,186),'Geotérmica':(160,181,213),'Marina':(81,169,189)}
-
-
 def descargar_archivo(url,ubicacion):
     """
     Esta función se descarga el archivo desde la web y lo guarda en una ubicación específica.
