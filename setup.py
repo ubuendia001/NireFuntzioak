5554156a01a86f85ddd36fe0480b208783d1fdcb
ubuendia001 <urkobuendia@gmail.com>
2024-03-07 09:34:16 +0100

    setUp

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="funtzioak",
+    version="1.0.0",
+    description="Erabiltzen ditudan funtzioak",
+    author="Urko B",
+    packages=find_packages(),
+    install_requires=[]
+)
